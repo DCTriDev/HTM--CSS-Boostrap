0cbad2f6d102430db8e02e05e61b89ecbc859ba9
tidun2 <donganton3@gmail.com>
2022-01-05 13:30:11 +0700

    layout farm-no responsive- no dark

--- Framstead/js/script.js
+++ Framstead/js/script.js
@@ -1,13 +1,21 @@
-// Slick
+// Slick Header
+// Slick Header
 $(".images-inner").slick({
   autoplay: true,
   autoplaySpeed: 2000,
   dots: true,
-
   infinite: true,
   prevArrow: "none",
   nextArrow: "none",
 });
+// Slick Testimonials
+$(".owl-stage").slick({
+  autoplay: false,
+  dots: true,
+  infinite: true,
+  arrows: false,
+});
+
 // Sticky header
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
@@ -17,13 +25,4 @@ $(window).scroll(function () {
   } else {
     $("#site-header").removeClass("whiteHeader");
   }
-});
-// header-search
-var btnSearchEl = document.querySelector("header .header-search .btn-material");
-btnSearchEl.addEventListener("click", function () {
-  document.getElementsByTagName("body")[0].classList.add("search-active");
-});
-var btnCloseEl = document.querySelector("header .header-search .icon-close");
-btnCloseEl.addEventListener("click", function () {
-  document.getElementsByTagName("body")[0].classList.remove("search-active");
 });
