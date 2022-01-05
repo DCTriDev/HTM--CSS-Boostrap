7153d33e76e2cef1e8f7ba657192e80c11b37b39
tidun2 <donganton3@gmail.com>
2022-01-05 23:17:39 +0700

    back-to-top

--- Framstead/js/script.js
+++ Framstead/js/script.js
@@ -1,5 +1,4 @@
 // Slick Header
-// Slick Header
 $(".images-inner").slick({
   autoplay: true,
   autoplaySpeed: 2000,
@@ -19,10 +18,23 @@ $(".owl-stage").slick({
 // Sticky header
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
-
   if (scroll >= 100) {
     $("#site-header").addClass("whiteHeader");
   } else {
     $("#site-header").removeClass("whiteHeader");
   }
+});
+// btn scroll top
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#movetop").fadeIn();
+    } else {
+      $("#movetop").fadeOut();
+    }
+  });
+  $("#movetop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
