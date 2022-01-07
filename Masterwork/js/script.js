b9f0e8d770a10cfbf724b521659a266da8c76a26
DCTriDev <dinhcongtri0905@gmail.com>
2022-01-07 14:13:28 +0700

    done html css masterwork, not responsive yet

--- Masterwork/js/script.js
+++ Masterwork/js/script.js
@@ -9,4 +9,16 @@ function scrollFunction() {
     } else {
         header_fix.classList.remove('nav-fix')
     }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
+
+document.querySelector('#back-to-top').addEventListener('click', function () {
+    topFunction()
+})
+function topFunction() {
+    window.scrollTo({top: 0, behavior: "smooth"})
 }
