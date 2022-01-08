df601b72438d2dc471a14c3131c7c1f176a05e53
tidun2 <donganton3@gmail.com>
2022-01-08 16:22:51 +0700

    thieu lasted news-footer

--- Corpvision/js/script.js
+++ Corpvision/js/script.js
@@ -26,3 +26,28 @@ var btnCloseEl = document.querySelector("header .header-search .icon-close");
 btnCloseEl.addEventListener("click", function () {
   document.getElementsByTagName("body")[0].classList.remove("search-active");
 });
+
+// owl carousel
+$(document).ready(function () {
+  $("#owl-demo1").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      768: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: false,
+        loop: false
+      }
+    }
+  })
+})
