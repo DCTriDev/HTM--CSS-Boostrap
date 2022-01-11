3de200c2689fa918a94b9ad8465d88e7a4205090
DCTriDev <dinhcongtri0905@gmail.com>
2022-01-11 20:35:26 +0700

    reponsive, done dark mode.

--- PoochCare/js/script.js
+++ PoochCare/js/script.js
@@ -3,6 +3,8 @@ window.onscroll = function () {
 };
 let header = document.querySelector("header")
 let backToTop = document.getElementById("back-to-top")
+let btnDarkMode = document.querySelector('#btn-dark-mode')
+
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 
@@ -19,4 +21,31 @@ function scrollFunction() {
 
 backToTop.addEventListener("click", function () {
     window.scrollTo({top: 0, behavior: "smooth"})
+})
+
+
+btnDarkMode.addEventListener('click', function () {
+    //Toggle icon
+    if (btnDarkMode.classList.contains('dark-mode-active')) {
+        btnDarkMode.classList.remove('dark-mode-active')
+    } else {
+        btnDarkMode.classList.add('dark-mode-active')
+    }
+
+    //Active darkmode
+
+    if (btnDarkMode.classList.contains('dark-mode-active')) {
+        document.documentElement.style.setProperty('--bg-color', '#151515');
+        document.documentElement.style.setProperty('--bg-light', '#222');
+        document.documentElement.style.setProperty('--heading-color', '#DDD');
+        document.documentElement.style.setProperty('--font-color', '#AFAFAF');
+        document.documentElement.style.setProperty('--border-light', '#444');
+        document.documentElement.style.setProperty('--border-color', '#333');
+    } else {
+        document.documentElement.style.setProperty('--bg-color', '#FFF');
+        document.documentElement.style.setProperty('--bg-light', 'rgba(147, 107, 232, 0.05)');
+        document.documentElement.style.setProperty('--heading-color', '#00c04');
+        document.documentElement.style.setProperty('--border-light', '#F0F3F0');
+        document.documentElement.style.setProperty('--border-color', '#DDD');
+    }
 })
