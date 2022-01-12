208732a85bc1c69924d931b58524f9b954906aa3
DCTriDev <dinhcongtri0905@gmail.com>
2022-01-12 22:33:03 +0700

    done dark mode

--- Studious/js/script.js
+++ Studious/js/script.js
@@ -28,11 +28,28 @@ btn_dark_mode.addEventListener('click', function () {
     if (btn_dark_mode.classList.contains('dark-mode-active')) {
         document.documentElement.style.setProperty("--primary-color", "#4867B0");
         document.documentElement.style.setProperty("--bg-color", "#161C2D");
+        document.documentElement.style.setProperty("--bg-color-2", "transparent");
         document.documentElement.style.setProperty("--heading-color", "#FDFFFC");
+        document.documentElement.style.setProperty("--logo-color", "#FFF");
+        document.documentElement.style.setProperty("--bg-color-li", "rgb(168 176 195 / 11%)");
+        document.documentElement.style.setProperty("--font-color", "#B7B7B7");
+        document.documentElement.style.setProperty("--bg-light", "#121725");
+        document.documentElement.style.setProperty("--border-color-light", "rgba(255,255,255,0.2)");
+        document.documentElement.style.setProperty("--box-shadow-color", "hsla(222, 34%, 17%, 1)");
+        document.documentElement.style.setProperty("--border-color", "rgba(255,255,255,0.2)");
     }
     else {
         document.documentElement.style.setProperty("--primary-color", "#0D369F");
         document.documentElement.style.setProperty("--bg-color", "#FFF");
+        document.documentElement.style.setProperty("--bg-color-2", "#FFF");
         document.documentElement.style.setProperty("--heading-color", "#060606");
+        document.documentElement.style.setProperty("--logo-color", "#0D369F");
+        document.documentElement.style.setProperty("--bg-color-li", "rgb(13 54 159 / 9%)");
+        document.documentElement.style.setProperty("--font-color", "#707070");
+        document.documentElement.style.setProperty("--bg-light", "#F8F9FF");
+        document.documentElement.style.setProperty("--border-color-light", "#EEE");
+        document.documentElement.style.setProperty("--box-shadow-color", "rgb(4 20 69 / 10%)");
+        document.documentElement.style.setProperty("--border-color", "transparent");
+
     }
 })
