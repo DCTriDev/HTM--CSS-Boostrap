8429d6e9d19a9b96c7d2520f0a3697570ba95123
tidun2 <donganton3@gmail.com>
2022-01-15 17:14:01 +0700

    fix darkmode

--- Dinner/js/index.js
+++ Dinner/js/index.js
@@ -1,106 +1,102 @@
 // back to top
-let backToTop = document.getElementById("back-to-top")
+let backToTop = document.getElementById("back-to-top");
 backToTop.addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" })
-})
-
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 // owl carouse
 $(document).ready(function () {
-    $("#owl-agency").owlCarousel({
-        loop: true,
+  $("#owl-agency").owlCarousel({
+    loop: true,
+    nav: false,
+    margin: 50,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
         nav: false,
-        margin: 50,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            736: {
-                items: 1,
-                nav: false
-            }
-        }
-    })
-})
+      },
+      736: {
+        items: 1,
+        nav: false,
+      },
+    },
+  });
+});
 
 //--magnific popup-- >
 
 $(document).ready(function () {
-    $('.popup-with-zoom-anim').magnificPopup({
-        type: 'inline',
+  $(".popup-with-zoom-anim").magnificPopup({
+    type: "inline",
 
-        fixedContentPos: false,
-        fixedBgPos: true,
+    fixedContentPos: false,
+    fixedBgPos: true,
 
-        overflowY: 'auto',
+    overflowY: "auto",
 
-        closeBtnInside: true,
-        preloader: false,
+    closeBtnInside: true,
+    preloader: false,
 
-        midClick: true,
-        removalDelay: 300,
-        mainClass: 'my-mfp-zoom-in'
-    });
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-zoom-in",
+  });
 
-    $('.popup-with-move-anim').magnificPopup({
-        type: 'inline',
+  $(".popup-with-move-anim").magnificPopup({
+    type: "inline",
 
-        fixedContentPos: false,
-        fixedBgPos: true,
+    fixedContentPos: false,
+    fixedBgPos: true,
 
-        overflowY: 'auto',
+    overflowY: "auto",
 
-        closeBtnInside: true,
-        preloader: false,
+    closeBtnInside: true,
+    preloader: false,
 
-        midClick: true,
-        removalDelay: 300,
-        mainClass: 'my-mfp-slide-bottom'
-    });
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-slide-bottom",
+  });
 });
 
 //-- //magnific popup -->
 
 // dark mode
 
-let btn_dark_mode = document.querySelector('#btn-dark-mode')
+let btn_dark_mode = document.querySelector("#btn-dark-mode");
 
-btn_dark_mode.addEventListener('click', function () {
-    //switch icon
-    btn_dark_mode.classList.toggle('dark-mode-active')
+btn_dark_mode.addEventListener("click", function () {
+  //switch icon
+  btn_dark_mode.classList.toggle("dark-mode-active");
 
-    //Color when dark mode active
-    if (btn_dark_mode.classList.contains('dark-mode-active')) {
-        document.documentElement.style.setProperty("--bg-color", "#232220");
-        document.documentElement.style.setProperty("--heading-color", "#eee");
-        document.documentElement.style.setProperty("--font-color", "#eee");
-        document.documentElement.style.setProperty("--bg-grey", "#282828");
-        document.documentElement.style.setProperty("--border-color-light", "rgba(255, 255, 255, 0.1)");
-        document.documentElement.style.setProperty("--bg-light", "#282828)");
+  //Color when dark mode active
+  if (btn_dark_mode.classList.contains("dark-mode-active")) {
+    document.documentElement.style.setProperty("--bg-color", "#232220");
+    document.documentElement.style.setProperty("--heading-color", "#eee");
+    document.documentElement.style.setProperty("--font-color", "#eee");
+    document.documentElement.style.setProperty("--bg-grey", "#282828");
+    document.documentElement.style.setProperty(
+      "--border-color-light",
+      "rgba(255, 255, 255, 0.1)"
+    );
+    document.documentElement.style.setProperty("--bg-light", "#282828)");
+    document.querySelector("body").style.backgroundColor = "#232220";
+  }
 
-
-
-
-
-
-
-
-    }
-
-    //   color when light mode active
-    else {
-        document.documentElement.style.setProperty("--bg-color", "#fff");
-        document.documentElement.style.setProperty("--heading-color", "#232220");
-        document.documentElement.style.setProperty("--font-color", "rgba(22, 28, 45, 0.7)");
-        document.documentElement.style.setProperty("--bg-grey", "#eee");
-        document.documentElement.style.setProperty("--border-color-light", "#eee");
-        document.documentElement.style.setProperty("--bg-light", "rgba(130, 115, 252, .05)");
-
-
-
-
-
-    }
-})                             
+  //   color when light mode active
+  else {
+    document.documentElement.style.setProperty("--bg-color", "#fff");
+    document.documentElement.style.setProperty("--heading-color", "#232220");
+    document.documentElement.style.setProperty(
+      "--font-color",
+      "rgba(22, 28, 45, 0.7)"
+    );
+    document.documentElement.style.setProperty("--bg-grey", "#eee");
+    document.documentElement.style.setProperty("--border-color-light", "#eee");
+    document.documentElement.style.setProperty(
+      "--bg-light",
+      "rgba(130, 115, 252, .05)"
+    );
+  }
+});
