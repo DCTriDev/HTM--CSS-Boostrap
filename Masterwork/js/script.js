69ab71bc0ec50f41608b66fec84abdefaab3d0a3
DCTriDev <dinhcongtri0905@gmail.com>
2022-01-16 10:29:22 +0700

    fix dark mode btn and change font color

--- Masterwork/js/script.js
+++ Masterwork/js/script.js
@@ -26,30 +26,35 @@ btn_dark_mode.addEventListener("click", function () {
   //switch icon
   btn_dark_mode.classList.toggle("dark-mode-active");
 
-  //Background when dark mode active
   if (btn_dark_mode.classList.contains("dark-mode-active")) {
     document.documentElement.style.setProperty("--bg-color", "#131313");
-    document.documentElement.style.setProperty("--primary", "#fffffe");
-    document.documentElement.style.setProperty("--title-color", "#94a1b2");
+    document.documentElement.style.setProperty("--primary", "#DC4F72");
+    document.documentElement.style.setProperty("--title-color", "#AFAFAF");
     document.documentElement.style.setProperty("--card-background", "#242629");
     document.documentElement.style.setProperty("--testi-color", "#242629");
-    document.documentElement.style.setProperty("--text-color", "#94a1b2");
+    document.documentElement.style.setProperty("--text-color", "#AFAFAF");
     document.documentElement.style.setProperty("--light-bg-color", "#232946");
     document.documentElement.style.setProperty("--box-shadow-color", "#625461");
+    document.documentElement.style.setProperty("--banner-color-h3", "#DC4F72");
+    document.documentElement.style.setProperty("--banner-color-des", "#131313");
+    document.documentElement.style.setProperty("--tool-item-hover", "#343A57");
+    document.documentElement.style.setProperty("--accordion-bg", "#343A57");
   }
 
-  //Background when light mode active
   else {
     document.documentElement.style.setProperty("--bg-color", "#FBFCFD");
     document.documentElement.style.setProperty("--primary", "#DC4F72");
     document.documentElement.style.setProperty("--title-color", "#2C3038");
     document.documentElement.style.setProperty("--text-color", "#5A5A5A");
-    document.documentElement.style.setProperty("--card-background", "#fff");
-    document.documentElement.style.setProperty("--testi-color", "#5a5a5a");
+    document.documentElement.style.setProperty("--card-background", "#FFF");
+    document.documentElement.style.setProperty("--testi-color", "#5A5A5A");
     document.documentElement.style.setProperty("--light-bg-color", "#F7F9FC");
     document.documentElement.style.setProperty(
-      "--box-shadow-color",
-      "rgba(0, 0, 0, 0.05)"
-    );
+        "--box-shadow-color",
+        "rgba(0, 0, 0, 0.05)");
+    document.documentElement.style.setProperty("--banner-color-h3", "#2c3038");
+    document.documentElement.style.setProperty("--banner-color-des", "#5a5a5a");
+    document.documentElement.style.setProperty("--tool-item-hover", "#FFF");
+    document.documentElement.style.setProperty("--accordion-bg", "#rgba(0,0,0,.03)");
   }
 });
